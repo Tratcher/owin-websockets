8cd45cc7d0beec960feb957bb8b4ed60dc57ad88
Tratcher <chrross@CHRROSS-DEV.redmond.corp.microsoft.com>
2012-09-07 13:19:36 -0700

    v0.3 Func update.

--- src/main/Owin.WebSockets/OwinWebSockets.cs
+++ src/main/Owin.WebSockets/OwinWebSockets.cs
@@ -1,51 +1,16 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Owin.WebSockets
 {
-    #pragma warning disable 811
     using WebSocketFunc =
         Func
         <
-            // SendAsync
-            Func
-            <
-                ArraySegment<byte> /* data */,
-                int /* messageType */,
-                bool /* endOfMessage */,
-                CancellationToken /* cancel */,
-                Task
-            >,
-            // ReceiveAsync
-            Func
-            <
-                ArraySegment<byte> /* data */,
-                CancellationToken /* cancel */,
-                Task
-                <
-                    Tuple
-                    <
-                        int /* messageType */,
-                        bool /* endOfMessage */,
-                        int? /* count */,
-                        int? /* closeStatus */,
-                        string /* closeStatusDescription */
-                    >
-                >
-            >,
-            // CloseAsync
-            Func
-            <
-                int /* closeStatus */,
-                string /* closeDescription */,
-                CancellationToken /* cancel */,
-                Task
-            >,
-            // Complete
-            Task
+            IDictionary<string, object>, // WebSocket environment
+            Task // Complete
         >;
-    #pragma warning restore 811
 
     using WebSocketSendAsync =
         Func
